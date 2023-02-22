f17ad4b276cb550e7c9d3e7633066f428b75347d
tomas <tomasdc2016@gmail.com>
2023-02-22 14:32:44 -0300

    fix: linter

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,8 @@ import connectToDatabase from './Models/Connection';
 const PORT = process.env.PORT || 3001;
 connectToDatabase()
   .then(() => {
-    app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
+    app.listen(PORT, () => PORT);
   })
-  .catch((error) => {
-    console.log('Connection with database generated an error:\r\n');
-    console.error(error);
-    console.log('\r\nServer initialization cancelled');
+  .catch(() => {
     process.exit(0);
   });
